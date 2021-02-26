43bfaf035af225444213ab9d37ffa80673f0eb35
lighnio <charlesvas16@outlook.com>
2021-02-25 22:41:29 -0600

    some warnings fixed

--- src/main.java
+++ src/main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class main {
 
-    public main(String[] args) {
+    public static void main(String[] args) {
 
         //Scanner
         Scanner entry = new Scanner(System.in);
